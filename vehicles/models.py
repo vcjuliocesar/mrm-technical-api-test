af5ea8bef02ce145fa38ec9f5ddbbf8fc3409f5a
julio cesar <vcjuliocesar@outlook.com>
2025-01-18 17:43:32 -0600

    config project

--- vehicles/models.py
+++ vehicles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Vehicle(models.Model):
+
+    brand = models.CharField(max_length = 100)
+    name = models.CharField(max_length = 100)
+    year = models.PositiveSmallIntegerField()
+    vehicle_model = models.CharField(max_length = 100)
+    price = models.DecimalField(max_digits = 10,decimal_places=2)
+    status = models.BooleanField(default=True)
