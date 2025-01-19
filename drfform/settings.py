f0375e7bdb7e97594f1a9f528782c6a701a3eef8
julio cesar <vcjuliocesar@outlook.com>
2025-01-18 21:19:14 -0600

    database config

--- drfform/settings.py
+++ drfform/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'drfform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_vehicles',
+        'USER':'admin',
+        'PASSWORD':'admin123',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
